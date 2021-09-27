68d74b63f6879ff486d356fb94ce2504762f93ec
rofe <github@rofe.com>
2021-09-27 07:45:22 +0200

    feat: support code api

--- src/sidekick/app.js
+++ src/sidekick/app.js
@@ -702,7 +702,8 @@
       id: 'publish',
       condition: (sidekick) => sidekick.isHelix() && sidekick.config.outerHost
         && !(sidekick.config.byocdn && sidekick.location.host === sidekick.config.host)
-        && (sidekick.status.edit && sidekick.status.edit.url), // show if edit url exists
+        && (sidekick.status.edit && sidekick.status.edit.url) // show if edit url exists
+        && sk.isContent(),
       button: {
         action: async (evt) => {
           const { config, location } = sk;
@@ -749,7 +750,8 @@
       condition: (sidekick) => sidekick.isHelix() && sidekick.config.outerHost
         && !(sidekick.config.byocdn && sidekick.location.host === sidekick.config.host)
         && (!sidekick.status.edit || !sidekick.status.edit.url) // show if no edit url
-        && sidekick.status.live && sidekick.status.live.lastModified, // show if published
+        && sidekick.status.live && sidekick.status.live.lastModified // show if published
+        && sk.isContent(),
       button: {
         action: async () => {
           const { status } = sk;
@@ -917,7 +919,9 @@
       }
       if (!this.status.apiUrl) {
         const { href, pathname } = this.location;
-        const apiUrl = getAdminUrl(this.config, 'preview', this.isEditor() ? '/' : pathname);
+        const apiUrl = getAdminUrl(
+          this.config, this.isContent() ? 'preview' : 'code', this.isEditor() ? '/' : pathname,
+        );
         if (this.isEditor()) {
           apiUrl.search = new URLSearchParams([
             ['editUrl', href],
@@ -1169,6 +1173,16 @@
     }
 
     /**
+     * Checks if the current location is a content URL.
+     * @returns {boolean} <code>true</code> if content URL, else <code>false</code>
+     */
+    isContent() {
+      const file = this.status.webPath && this.status.webPath.split('/').pop();
+      const ext = file && file.split('.').pop();
+      return ext === file || ext === 'html' || ext === 'json';
+    }
+
+    /**
      * Displays a non-sticky notification.
      * @param {string|string[]} msg The message (lines) to display
      * @param {number}          level error (0), warning (1), of info (2)
@@ -1312,7 +1326,7 @@
       } else {
         envUrl = `https://${config[hostType]}${status.webPath}`;
         if (config.hlx3 && targetEnv === 'preview' && this.isEditor()) {
-          await this.update(status.webPath);
+          await this.update();
         }
       }
       if (!envUrl) {
@@ -1334,7 +1348,7 @@
     }
 
     /**
-     * Updates the preview of the current page.
+     * Updates the preview or code of the current resource.
      * @fires Sidekick#updated
      * @return {Response} The response object
      */
@@ -1345,11 +1359,12 @@
       try {
         if (config.hlx3) {
           // update preview
-          resp = await fetch(getAdminUrl(config, 'preview', path), { method: 'POST' });
+          resp = await fetch(getAdminUrl(config, this.isContent() ? 'preview' : 'code', path),
+            { method: 'POST' });
         } else {
           resp = await this.publish(path, true);
         }
-        if (this.isEditor() || this.isInner() || this.isDev()) {
+        if ((this.isEditor() || this.isInner() || this.isDev()) && this.isContent()) {
           // bust client cache
           await fetch(`https://${config.innerHost}${path}`, { cache: 'reload', mode: 'no-cors' });
         }
@@ -1365,7 +1380,7 @@
     }
 
     /**
-     * Deletes the preview resource.
+     * Deletes the preview or code of the current resource.
      * @fires Sidekick#deleted
      * @return {Response} The response object
      */
@@ -1376,7 +1391,8 @@
       try {
         if (config.hlx3) {
           // delete preview
-          resp = await fetch(getAdminUrl(config, 'preview', path), { method: 'DELETE' });
+          resp = await fetch(getAdminUrl(config, this.isContent() ? 'preview' : 'code', path),
+            { method: 'DELETE' });
           if (status.live && status.live.lastModified) {
             await this.unpublish(path);
           }
@@ -1414,7 +1430,8 @@
       const { config, location } = this;
 
       if ((!innerOnly && !config.hlx3 && !config.host) // non-hlx3 without host
-        || (config.byocdn && location.host === config.host)) { // byocdn and prod host
+        || (config.byocdn && location.host === config.host) // byocdn and prod host
+        || !this.isContent()) {
         return null;
       }
 
@@ -1466,6 +1483,9 @@
      * @return {Response} The response object
      */
     async unpublish() {
+      if (!this.isContent()) {
+        return null;
+      }
       const { config, status } = this;
       const path = status.webPath;
       let resp;
