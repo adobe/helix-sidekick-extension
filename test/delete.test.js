84c9620420438b676d3b6a08fdbce9b9ecb8bfb8
rofe <github@rofe.com>
2021-09-27 13:41:27 +0200

    testL support code api

--- test/delete.test.js
+++ test/delete.test.js
@@ -173,6 +173,49 @@ describe('Test delete plugin', () => {
     assert.ok(redirected, 'Redirect to homepage not triggered');
   }).timeout(IT_DEFAULT_TIMEOUT);
 
+  it('Delete plugin uses code API in hlx3 mode', async () => {
+    const page = getPage();
+    const apiMock = { ...MOCKS.api.blogCode };
+    delete apiMock.edit;
+    let apiCalled = false;
+    let redirected = false;
+    // wait for delete confirmation dialog and accept it
+    page.on('dialog', async (dialog) => {
+      if (dialog.type() === 'confirm') {
+        assert.ok(
+          dialog.message().includes('Are you sure you want to delete it?'),
+          `Unexpected dialog message: "${dialog.message()}"`,
+        );
+        dialog.accept();
+      }
+    });
+    await testPageRequests({
+      page,
+      url: `${fixturesPrefix}/reload-staging-code-hlx3.html`,
+      check: (req) => {
+        if (!apiCalled && req.method() === 'DELETE') {
+          // intercept api request
+          apiCalled = req.url().endsWith(`/code/adobe/theblog/master${apiMock.webPath}`);
+        } else if (req.url() === 'https://master--theblog--adobe.hlx3.page/') {
+          // reload triggered
+          redirected = true;
+          return true;
+        }
+        return false;
+      },
+      checkCondition: (request) => request.url().startsWith('https://')
+        || request.url().endsWith('reload-staging-code-hlx3.html'),
+      mockResponses: [
+        apiMock,
+        'deleted',
+      ],
+      plugin: 'delete',
+    });
+    // check result
+    assert.ok(apiCalled, 'Code API not called');
+    assert.ok(redirected, 'Redirect to homepage not triggered');
+  }).timeout(IT_DEFAULT_TIMEOUT);
+
   it('No delete plugin if source document exists', async () => {
     const page = getPage();
     await mockStandardResponses(page, {
