6f6d0d7d7b6a02347def47d6f93faee0c98af3e4
rofe <github@rofe.com>
2021-11-28 10:36:09 +0100

    fix: preserve hash when switching env

--- src/sidekick/module.js
+++ src/sidekick/module.js
@@ -1402,14 +1402,14 @@
       if (this.status.error) {
         return this;
       }
-      const { config, location: { href, search }, status } = this;
+      const { config, location: { href, search, hash }, status } = this;
       this.showModal('Please wait …', true);
       if (!status.webPath) {
         console.log('not ready yet, trying again in a second ...');
         window.setTimeout(() => this.switchEnv(targetEnv, open), 1000);
         return this;
       }
-      const envUrl = `https://${config[hostType]}${status.webPath}${search}`;
+      const envUrl = `https://${config[hostType]}${status.webPath}${search}${hash}`;
       if (config.hlx3 && targetEnv === 'preview' && this.isEditor()) {
         await this.update();
       }
