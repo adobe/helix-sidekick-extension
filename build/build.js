a660e94161e8af14fe37ebbcf9ea8b8fd914b4f6
rofe <github@rofe.com>
2021-12-20 08:39:19 +0100

    feat(extension): build tooling for different browsers

---
+++ build/build.js
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* eslint-disable no-console, import/no-extraneous-dependencies */
+
+const fs = require('fs-extra');
+const archiver = require('archiver');
+
+const supportedBrowsers = ['chrome', 'safari', 'firefox'];
+
+function copyResources(browser) {
+  const sourceDir = './src/extension';
+  const targetDir = `./dist/${browser}`;
+  try {
+    fs.ensureDirSync(targetDir);
+    fs.copySync(sourceDir, targetDir, {
+      overwrite: true,
+      filter: (src) => src.split('/').pop() !== 'manifest.json',
+    });
+  } catch (e) {
+    console.error(`  failed to copy resources: ${e.message}`);
+    process.exit(1);
+  }
+  console.log(`  resources copied to ${targetDir}`);
+}
+
+function copyManifestKeys(sourceObj, browser) {
+  const targetObj = {};
+  Object.keys(sourceObj).forEach((sourceKey) => {
+    let targetKey = sourceKey;
+    if (sourceKey.startsWith('__')) {
+      // only copy key if prefix matches browser
+      if (sourceKey.startsWith(`__${browser}__`)) {
+        targetKey = sourceKey.split('__').pop();
+      } else {
+        return;
+      }
+    }
+    if (typeof sourceObj[sourceKey] === 'object') {
+      if (Array.isArray(sourceObj[sourceKey])) {
+        targetObj[targetKey] = sourceObj[sourceKey].map((key) => key);
+      } else {
+        targetObj[targetKey] = copyManifestKeys(sourceObj[sourceKey], browser);
+      }
+    } else {
+      targetObj[targetKey] = sourceObj[sourceKey];
+    }
+  });
+  return targetObj;
+}
+
+function buildManifest(browser) {
+  const targetPath = `./dist/${browser}/manifest.json`;
+  let targetMF = {};
+  try {
+    const sourceMF = JSON.parse(fs.readFileSync('./src/extension/manifest.json'));
+    targetMF = copyManifestKeys(sourceMF, browser);
+  } catch (e) {
+    console.error(`  failed to read source manifest.json: ${e.message}`);
+    process.exit(1);
+  }
+  try {
+    fs.ensureFileSync(targetPath);
+    fs.writeFileSync(targetPath, JSON.stringify(targetMF, null, '  '), { encoding: 'utf-8' });
+  } catch (e) {
+    console.error(`  failed to write target manifest.json: ${e.message}`);
+    process.exit(1);
+  }
+  console.log(`  ${browser}-specific manifest.json created at ${targetPath}`);
+}
+
+function zipExtension(browser) {
+  const dir = `./dist/${browser}`;
+  const zip = `${dir}.zip`;
+  const output = fs.createWriteStream(zip);
+  const archive = archiver('zip', {
+    zlib: { level: 9 }
+  });
+  archive.on('error', function(e) {
+    console.error(`  failed to zip extension: ${e.message}`);
+    process.exit(1);
+  });
+
+  archive.pipe(output);
+  archive.directory(dir, false);
+  archive.finalize();
+
+  console.log(`  zip created at ${zip}`);
+}
+
+// run build script
+const browser = process.argv[2];
+if (!browser) {
+  console.log(`specify one of the following browers: ${supportedBrowsers.join(', ')}`);
+  process.exit(0);
+}
+if (!supportedBrowsers.includes(browser)) {
+  console.error(`unsupported browser ${browser}`);
+  process.exit(1);
+}
+
+console.log(`building ${browser} extension...`);
+
+copyResources(browser);
+buildManifest(browser);
+
+if (browser === 'chrome') {
+  zipExtension(browser);
+}
+
+console.log('done.');
