09efe1752b803949e1063a124c18a23f3975835b
rofe <github@rofe.com>
2022-02-11 20:57:23 +0100

    chore: fail build step if browser missing [skip ci]

--- build/build.js
+++ build/build.js
@@ -97,7 +97,7 @@ function zipExtension(browser) {
 const browser = process.argv[2];
 if (!browser) {
   console.log(`specify one of the following browers: ${supportedBrowsers.join(', ')}`);
-  process.exit(0);
+  process.exit(1);
 }
 if (!supportedBrowsers.includes(browser)) {
   console.error(`unsupported browser ${browser}`);
