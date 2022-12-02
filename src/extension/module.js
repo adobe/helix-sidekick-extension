fd85d51f1fcf31587a536b918917b187d16c12a7
Dominique Pfister <dpfister@adobe.com>
2022-12-02 14:38:38 +0100

    fix: harmonize messages

--- src/extension/module.js
+++ src/extension/module.js
@@ -1192,14 +1192,25 @@
         action: async () => {
           sk.addEventListener('statusfetched', async () => {
             const selection = sk.getAdminSelection();
+            const numItems = `${selection.length} item${selection.length === 1 ? '' : 's'}`;
             if (selection.length === 0) {
-              sk.showModal('No or invalid selection found');
-            } else if (window.confirm(`Are you sure you want to preview ${selection.length} item${selection.length === 1 ? '' : 's'}?`)) {
+              sk.showModal('No valid selection found');
+            } else if (window.confirm(`Are you sure you want to preview ${numItems}?`)) {
               sk.showWait();
               const results = await Promise.all(
                 selection.map(async (file) => sk.update(file.path)),
               );
-              sk.showModal(['Selection previewed:', ...results.map((res) => `${res.path} (${res.ok ? 'OK' : `Failed: ${res.error}`})`)]);
+              const { config } = sk;
+              const host = config.innerHost;
+              navigator.clipboard.writeText(selection.map((item) => `https://${host}${item.path}`).join('\n'));
+              const msg = `${numItems} previewed and copied to clipboard`;
+              if (results.length <= 3) {
+                sk.showModal([`${msg}:`, ...results.map(
+                  (res) => `${res.path} (${res.ok ? 'OK' : `Failed: ${res.error}`})`,
+                )]);
+              } else {
+                sk.showModal(msg);
+              }
             }
           }, { once: true });
           sk.fetchStatus(true);
@@ -1222,9 +1233,10 @@
         action: async () => {
           sk.addEventListener('statusfetched', async () => {
             const selection = sk.getAdminSelection();
+            const numItems = `${selection.length} item${selection.length === 1 ? '' : 's'}`;
             if (selection.length === 0) {
-              sk.showModal('No or invalid selection found');
-            } else if (window.confirm(`Are you sure you want to publish ${selection.length} item${selection.length === 1 ? '' : 's'}?`)) {
+              sk.showModal('No valid selection found');
+            } else if (window.confirm(`Are you sure you want to publish ${numItems}?`)) {
               sk.showWait();
               const results = await Promise.all(
                 selection.map(async ({ path }) => {
@@ -1237,7 +1249,17 @@
                   };
                 }),
               );
-              sk.showModal(['Selection published:', ...results.map((res) => `${res.path} (${res.ok ? 'OK' : `Failed: ${res.error}`})`)]);
+              const { config } = sk;
+              const host = config.host || config.outerHost;
+              navigator.clipboard.writeText(selection.map((item) => `https://${host}${item.path}`).join('\n'));
+              const msg = `${numItems} published and copied to clipboard`;
+              if (results.length <= 3) {
+                sk.showModal([`${msg}:`, ...results.map(
+                  (res) => `${res.path} (${res.ok ? 'OK' : `Failed: ${res.error}`})`,
+                )]);
+              } else {
+                sk.showModal(msg);
+              }
             }
           }, { once: true });
           sk.fetchStatus(true);
