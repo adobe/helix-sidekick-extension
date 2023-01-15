ab3b618da9b774eeb2f8901037438f79b2c96371
rofe <github@rofe.com>
2023-01-16 00:25:11 +0100

    chore(safari): debug pre build script [skip ci]

--- src/safari/ci_scripts/ci_pre_xcodebuild.sh
+++ src/safari/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,7 +4,10 @@
 #  helix-sidekick-extension
 #
 
+echo node:`which node`
+echo brew:`which brew`
+echo port:`which port`
+echo pkgin:`which pkgin`
 cd ../../..
-pwd
-echo `which node`
-`which node` ./build/build.js safari
+curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+node ./build/build.js safari
