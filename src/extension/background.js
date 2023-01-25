3aa200c9aa8edec7c73bcb7eb089e59f291c8ad9
Alexandre Capt <acapt@adobe.com>
2023-01-25 11:33:47 +0100

    chore: support latest class format

--- src/extension/background.js
+++ src/extension/background.js
@@ -113,7 +113,7 @@ async function guessIfFranklinSite({ id }) {
       func: () => {
         const isFranklinSite = document.head.querySelectorAll('script[src*="scripts.js"]').length > 0
           && document.head.querySelectorAll('link[href*="styles.css"]').length > 0
-          && document.body.querySelector('main > div.section') !== null;
+          && (document.body.querySelector('main > div.section') !== null || document.body.querySelector('main > div.section-wrapper'));
         chrome.runtime.sendMessage({ isFranklinSite });
       },
     });
