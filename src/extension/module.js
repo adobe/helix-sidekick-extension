df63ea61b6a79b804e5009c457f42b64120becfc
rofe <github@rofe.com>
2023-03-15 16:36:01 +0100

    feat: add telemetry

--- src/extension/module.js
+++ src/extension/module.js
@@ -268,6 +268,75 @@
   const DEV_URL = new URL('http://localhost:3000');
 
   /**
+   * Log RUM for sidekick telemetry.
+   * @private
+   * @param {string} checkpoint identifies the checkpoint in funnel
+   * @param {Object} data additional data for RUM sample
+   */
+  function sampleRUM(checkpoint, data = {}) {
+    sampleRUM.defer = sampleRUM.defer || [];
+    const defer = (fnname) => {
+      sampleRUM[fnname] = sampleRUM[fnname]
+        || ((...args) => sampleRUM.defer.push({ fnname, args }));
+    };
+    sampleRUM.drain = sampleRUM.drain
+      || ((dfnname, fn) => {
+        sampleRUM[dfnname] = fn;
+        sampleRUM.defer
+          .filter(({ fnname }) => dfnname === fnname)
+          .forEach(({ fnname, args }) => sampleRUM[fnname](...args));
+      });
+    sampleRUM.on = (chkpnt, fn) => {
+      sampleRUM.cases[chkpnt] = fn;
+    };
+    defer('observe');
+    defer('cw');
+    try {
+      window.hlx = window.hlx || {};
+      const sk = window.hlx.sidekick;
+      if (!window.hlx.rum) {
+        const usp = new URLSearchParams(sk.location.search);
+        const weight = (usp.get('hlx-sk-rum') === 'on') ? 1 : 10; // with parameter, weight is 1. Defaults to 10.
+        // eslint-disable-next-line no-bitwise
+        const hashCode = (s) => s.split('').reduce((a, b) => (((a << 5) - a) + b.charCodeAt(0)) | 0, 0);
+        const id = `${hashCode(sk.location.href)}-${new Date().getTime()}-${Math.random().toString(16).substr(2, 14)}`;
+        const random = Math.random();
+        const isSelected = (random * weight < 1);
+        // eslint-disable-next-line object-curly-newline
+        window.hlx.rum = { weight, id, random, isSelected, sampleRUM };
+      }
+      const { weight, id } = window.hlx.rum;
+      if (window.hlx && window.hlx.rum && window.hlx.rum.isSelected) {
+        const sendPing = (pdata = data) => {
+          // eslint-disable-next-line object-curly-newline, max-len, no-use-before-define
+          const body = JSON.stringify({ weight, id, referer: sk.location.href, generation: window.hlx.RUM_GENERATION, checkpoint, ...data });
+          const url = `https://rum.hlx.page/.rum/${weight}`;
+          // eslint-disable-next-line no-unused-expressions
+          navigator.sendBeacon(url, body);
+          // eslint-disable-next-line no-console
+          console.debug(`ping:${checkpoint}`, pdata);
+        };
+        sampleRUM.cases = sampleRUM.cases || {
+          cwv: () => sampleRUM.cwv(data) || true,
+          lazy: () => {
+            // use classic script to avoid CORS issues
+            const script = document.createElement('script');
+            script.src = 'https://rum.hlx.page/.rum/@adobe/helix-rum-enhancer@^1/src/index.js';
+            document.head.appendChild(script);
+            return true;
+          },
+        };
+        sendPing(data);
+        if (sampleRUM.cases[checkpoint]) {
+          sampleRUM.cases[checkpoint]();
+        }
+      }
+    } catch (error) {
+      // something went wrong
+    }
+  }
+
+  /**
    * Retrieves project details from a host name.
    * @private
    * @param {string} host The host name
@@ -706,6 +775,27 @@
           },
         },
       }));
+      const userEvents = [
+        'updated',
+        'published',
+        'unpublished',
+        'deleted',
+        'envswitched',
+        'loggedin',
+        'loggedout',
+        'helpnext',
+        'helpdismissed',
+        'helpacknowlegded',
+        'helpoptedout',
+      ];
+      if (name.startsWith('custom:') || userEvents.includes(name)) {
+        // log telemetry
+        sampleRUM(`sidekick:${name}`, {
+          source: sk.location.href,
+          target: sk.status.webPath,
+          context: data || {},
+        });
+      }
     } catch (e) {
       console.warn('failed to fire event', name, e);
     }
