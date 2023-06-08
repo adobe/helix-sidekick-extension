87671c1c5ba7e012572b9acfd7a24839d00ec8cd
rofe <github@rofe.com>
2023-06-08 21:25:53 +0200

    chore: fix build script [skip ci]

--- src/safari/ci_scripts/ci_pre_xcodebuild.sh
+++ src/safari/ci_scripts/ci_pre_xcodebuild.sh
@@ -12,5 +12,5 @@ npm run build:safari
 if [ $CI_BRANCH != "main" ]; then
   echo "Building from branch $CI_BRANCH, use test icon"
   rm -rf $APP_ICON_PATH
-  mv "$CI_WORKSPACE/ci_scripts/AppIcon-Test.appiconset" $APP_ICON_PATH
+  mv "$CI_WORKSPACE/src/safari/ci_scripts/AppIcon-Test.appiconset" $APP_ICON_PATH
 fi
