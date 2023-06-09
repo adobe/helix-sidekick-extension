2d80e95a891d0586391147b318b7e17d5fad137a
rofe <github@rofe.com>
2023-06-09 08:21:20 +0200

    chore: debug logging

--- src/safari/ci_scripts/ci_pre_xcodebuild.sh
+++ src/safari/ci_scripts/ci_pre_xcodebuild.sh
@@ -11,7 +11,7 @@ npm run build:safari
 
 if [ $CI_BRANCH != "main" ]; then
   echo "Building from branch $CI_BRANCH, use test icon"
-  echo "$CI_WORKSPACE/src/safari/ci_scripts/AppIcon-Test.appiconset > $APP_ICON_PATH
+  echo $CI_WORKSPACE/src/safari/ci_scripts/AppIcon-Test.appiconset > $APP_ICON_PATH
   rm -rf $APP_ICON_PATH
-  mv "$CI_WORKSPACE/src/safari/ci_scripts/AppIcon-Test.appiconset" $APP_ICON_PATH
+  mv $CI_WORKSPACE/src/safari/ci_scripts/AppIcon-Test.appiconset $APP_ICON_PATH
 fi
