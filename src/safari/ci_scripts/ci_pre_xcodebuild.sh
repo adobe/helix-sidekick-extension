f34885a7a707a3251c8fa781080e6a8b916a8c30
rofe <github@rofe.com>
2023-06-11 22:29:29 +0200

    chore: finalize bash script [skip ci]

--- src/safari/ci_scripts/ci_pre_xcodebuild.sh
+++ src/safari/ci_scripts/ci_pre_xcodebuild.sh
@@ -10,19 +10,19 @@ npm install
 npm run build:safari
 
 if [ $CI_BRANCH != "main" ]; then
+  echo "Building from branch $CI_BRANCH"
+
   # use test version
   PBX_FILE="$CI_PROJECT_FILE_PATH/project.pbxproj"
   VERSION=`cat "$CI_WORKSPACE/src/extension/manifest.json" | grep -e "\"version\"" | sed -n "s/[^0-9.]//gp"`
   TEST_VERSION=`$CI_WORKSPACE/src/safari/ci_scripts/increment_version.sh -p $VERSION`
-  echo "VERSION: $VERSION"
-  echo "TEST_VERSION: $TEST_VERSION"
   sed -i "" "s/$VERSION/$TEST_VERSION/g" "$PBX_FILE"
-  echo `cat "$PBX_FILE" | grep MARKETING_VERSION`
+  echo "using test version $TEST_VERSION"
 
   # use test icon
   DEFAULT_ICON="$CI_WORKSPACE/src/safari/Shared (App)/Assets.xcassets/AppIcon.appiconset"
   TEST_ICON="$CI_WORKSPACE/src/safari/ci_scripts/AppIcon-Test.appiconset"
-  echo "Building from branch $CI_BRANCH, use test icon"
   rm -rf "$DEFAULT_ICON"
   mv "$TEST_ICON" "$DEFAULT_ICON"
+  echo "using test icon"
 fi
