57becd3632ed9cb98f10f46015c23fcdf001655d
rofe <github@rofe.com>
2023-06-11 14:01:03 +0200

    chore: use semver test version [skip ci]

--- src/safari/ci_scripts/ci_pre_xcodebuild.sh
+++ src/safari/ci_scripts/ci_pre_xcodebuild.sh
@@ -11,8 +11,13 @@ npm run build:safari
 
 if [ $CI_BRANCH != "main" ]; then
   # use test version
+  echo "CI_PROJECT_FILE_PATH: $CI_PROJECT_FILE_PATH"
   PBX_FILE="$CI_WORKSPACE/src/safari/helix-sidekick-extension.xcodeproj/project.pbxproj"
-  sed -E -i "" "s/[0-9]+\.[0-9]+\.[0-9]+/99.99.99/g" "$PBX_FILE"
+  VERSION=`cat "$CI_WORKSPACE/src/extension/manifest.json" | jq -r ".version"`
+  TEST_VERSION=`./increment_version.sh -p $VERSION`
+  echo "VERSION: $VERSION"
+  echo "TEST_VERSION: $TEST_VERSION"
+  sed -E -i "" "$VERSION/$TEST_VERSION/g" "$PBX_FILE"
   echo `cat "$PBX_FILE" | grep MARKETING_VERSION`
 
   # use test icon
