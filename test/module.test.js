a0f2678714f89d3ab8265d4a92d371605a6dda14
rofe <github@rofe.com>
2023-08-02 09:25:26 +0200

    chore: make info plugin test more resilient

--- test/module.test.js
+++ test/module.test.js
@@ -796,7 +796,7 @@ describe('Test sidekick', () => {
             .innerText),
         }).run();
         assert.ok(
-          checkPageResult.includes('Jun 18, 2021'),
+          checkPageResult.includes('Jun 18, 2021') || checkPageResult.includes('18 Jun 2021'),
           `Dates not displayed by info plugin: ${checkPageResult}`,
         );
       }).timeout(IT_DEFAULT_TIMEOUT);
