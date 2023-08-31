6e5efd2d7b35f4b16af47e50659d8059d108d7ba
Alexandre Capt <acapt@adobe.com>
2023-08-31 09:27:45 -0400

    fix: urls in json rendition

--- src/extension/view/json/json.js
+++ src/extension/view/json/json.js
@@ -24,7 +24,7 @@ function drawHeader(table, rowData) {
   thead.append(row);
 }
 
-function drawValue(cell, value) {
+function drawValue(cell, value, url) {
   const valueContainer = cell.appendChild(document.createElement('div'));
   if (value && !Number.isNaN(+value)) {
     // check for date
@@ -43,14 +43,15 @@ function drawValue(cell, value) {
   } else if (value.startsWith('/') || value.startsWith('http')) {
     // assume link
     const link = valueContainer.appendChild(document.createElement('a'));
-    link.href = value;
+    const target = new URL(value, url).toString();
+    link.href = target;
     link.title = value;
     link.target = '_blank';
     if (value.includes('media_')) {
       // linked image
       valueContainer.classList.add('image');
       const img = link.appendChild(document.createElement('img'));
-      img.src = value;
+      img.src = target;
     } else {
       // text link
       link.textContent = value;
@@ -69,20 +70,20 @@ function drawValue(cell, value) {
   }
 }
 
-function drawBody(table, data) {
+function drawBody(table, data, url) {
   const tbody = table.appendChild(document.createElement('tbody'));
   data.forEach((set) => {
     const row = document.createElement('tr');
     Object.keys(set).forEach((key) => {
       const cell = document.createElement('td');
-      drawValue(cell, set[key]);
+      drawValue(cell, set[key], url);
       row.append(cell);
     });
     tbody.append(row);
   });
 }
 
-export default function draw(json) {
+export default function draw(json, url) {
   const sheets = {};
   if (json[':type'] === 'multi-sheet' && json[':names']) {
     json[':names'].forEach((name) => {
@@ -103,7 +104,7 @@ export default function draw(json) {
     title.textContent = name;
     const table = document.body.appendChild(document.createElement('table'));
     drawHeader(table, sheet[0]);
-    drawBody(table, sheet);
+    drawBody(table, sheet, url);
   });
 }
 
@@ -120,7 +121,7 @@ export default function draw(json) {
         } catch (e) {
           throw new Error(`invalid json found at ${url}`);
         }
-        draw(json);
+        draw(json, url);
       } else {
         throw new Error(`failed to load ${url}: ${res.status}`);
       }
