8b38c58085d1c66e50a3d398072d1e65399c17da
Alexandre Capt <acapt@adobe.com>
2023-11-02 14:10:56 +0100

    chore: align name

--- src/extension/view-doc-source/js/ui.js
+++ src/extension/view-doc-source/js/ui.js
@@ -222,7 +222,7 @@ const load = async () => {
   const copyButton = document.getElementById('copy');
   copyButton.addEventListener('click', () => {
     copyHTMLToClipboard(editor.innerHTML);
-    copyButton.innerHTML = chrome.i18n.getMessage('doc_source_copied');
+    copyButton.innerHTML = chrome.i18n.getMessage('view_doc_source_copied');
     copyButton.classList.add('copied');
     setTimeout(() => {
       copyButton.innerHTML = chrome.i18n.getMessage('copy');
